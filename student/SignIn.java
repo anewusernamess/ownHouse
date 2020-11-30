5a82fe7b29c1b8871c14291f3c2f5c9938c416d7
wangmeng <sile@qq.com>
2020-11-30 17:13:26 +0800

    在家

---
+++ student/SignIn.java
@@ -0,0 +1,13 @@
+/*
+* @Author: Marte
+* @Date:   2020-11-30 17:01:48
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-11-30 17:11:27
+*/
+
+public class java {
+    public static void main(String[] args) {
+        System.out.println("id-"+id);
+
+    }
+}
