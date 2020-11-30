e5f43a2064f527df39e49f98cb6de8dd9698070e
zs <zs@qq.com>
2020-11-30 17:09:31 +0800

    在公司开发签到功能

---
+++ student/SignIn.java
@@ -0,0 +1,15 @@
+/*
+* @Author: Marte
+* @Date:   2020-11-30 16:50:15
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-11-30 17:08:14
+*/
+
+public class java {
+    public static void main(String[] args) {
+        private int id;
+        public void sign(){
+
+        }
+    }
+}
